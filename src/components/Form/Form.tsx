416ff7dee998dcb7a76d21b86bbc5aeefdb3cc4c
CalderonCodes <cgcalderondev@gmail.com>
2025-08-14 13:11:02 -0600

    :feat: handle step change

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,28 +1,61 @@
-import StepCard from '../StepCard/StepCard'
-import StepFour from '../StepFour/StepFour'
-import StepOne from '../StepOne/StepOne'
-import StepThree from '../StepThree/StepThree'
-import StepTwo from '../StepTwo/StepTwo'
-import './Form.scss'
+import { useReducer, useState } from "react";
+import StepCard from "../StepCard/StepCard";
+import StepFour from "../StepFour/StepFour";
+import StepOne from "../StepOne/StepOne";
+import StepThree from "../StepThree/StepThree";
+import StepTwo from "../StepTwo/StepTwo";
+import "./Form.scss";
+
+const updateStep = (state: number, type: "Next" | "Prev") => {
+  switch (type) {
+    case "Next":
+      return state + 1;
+    case "Prev":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function Form() {
+  const [step, dispatchStep] = useReducer(updateStep, 1);
+  console.log(step);
+
   return (
     <div className="container">
-        <div className='container_list'>
-            <StepCard/>
-            <StepCard/>
-            <StepCard/>
-            <StepCard/>
-        </div>
-        <div className='container_form'>
-            <StepFour/>
-        <div className="steps">
-        <button className="steps_prev">Go back</button>
-        <button className="steps_next">Next Step</button>
+      <div className="container_list">
+        <StepCard step={1} name="YOUR INFO" isActive={step===1} />
+        <StepCard step={2} name="SELECT PLAN"  isActive={step===2}/>
+        <StepCard step={3} name="ADD-ONS" isActive={step===3}/>
+        <StepCard step={4} name="SUMMARY" isActive={step===4}/>
       </div>
+      <div className="container_form">
+        {step === 1 ? (
+          <StepOne />
+        ) : step === 2 ? (
+          <StepTwo />
+        ) : step === 3 ? (
+          <StepThree />
+        ) : (
+          <StepFour />
+        )}
+        <div className="steps">
+          <button
+            className={`steps_prev ${step === 1 ? "hidden" : ""}`}
+            onClick={() => dispatchStep("Prev")}
+          >
+            Go back
+          </button>
+          <button
+            className={`steps_next ${step === 4 ? "hidden" : ""}`}
+            onClick={() => dispatchStep("Next")}
+          >
+            Next Step
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
--- src/components/StepCard/StepCard.tsx
+++ src/components/StepCard/StepCard.tsx
@@ -1,13 +1,20 @@
 import './StepCard.scss'
-function StepCard() {
+
+type StepProps ={
+  step: number,
+  name: string,
+  isActive: boolean
+}
+
+function StepCard({step,name, isActive}: StepProps) {
   return (
     <div className="stepcard">
-      <div className="stepcard_number">
-        <p>1</p>
+      <div className={`stepcard_number ${isActive? 'activeNumber' : ''} `}>
+        <p>{step}</p>
       </div>
       <div className="stepcard_details">
-        <p className="stepcard_details_number">STEP 1</p>
-        <p className="stepcard_details_name">SELECT PLAN</p>
+        <p className="stepcard_details_number">STEP {step}</p>
+        <p className="stepcard_details_name">{name}</p>
       </div>
     </div>
   );
